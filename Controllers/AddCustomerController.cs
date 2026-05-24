aab5d5bc1e5b4d53635adadafba9d5f512780483
gabs1san <sanbiel398@gmail.com>
2026-05-24 20:32:07 -0300

    Add project files.

---
+++ Controllers/AddCustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddCustomerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddCustomer(AddCustomerInput input)
+        {
+            
+        }
+    }
+}
---
+++ Models/AddCustumer/AddCustomerInput.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiWeb.Models.AddCostumer
+{
+    public class AddCustomerInput
+    {
+        public string name  { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Document { get; set; } = string.Empty; 
+    }
+}
